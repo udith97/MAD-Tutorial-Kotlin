03de748f7f64ade43331c459b922aba24f92247a
Udith Jayasinghe <42823912+udith97@users.noreply.github.com>
2020-05-22 19:01:15 +0530

    First Commit

---
+++ app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myfirstkotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun onSubmit(view: View) {
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        textView2.text="Age :" + (currentYear - editText.text.toString().toInt())
+    }
+}
